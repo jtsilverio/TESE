ccb038588c11c32faeb377b3777be7930d08fa84
Jefferson <jfo.txt@gmail.com>
2022-02-16 15:43:39 -0300

    added a script with libraries needed to knit the document

---
+++ requirements.R
@@ -0,0 +1,37 @@
+if (!require("devtools")) install.packages("devtools", repos = "http://cran.rstudio.org")
+devtools::install_github("rstudio/bookdown")
+devtools::install_github("COPPE-UFRJ/coppedown")
+
+# For knitting the main thesis
+install.packages('tinytex')
+tinytex::install_tinytex()
+install.packages("knitr")
+install.packages("ggplot2")
+install.packages("patchwork")
+install.packages("data.table")
+install.packages("dplyr")
+install.packages("momentuHMM")
+install.packages("maptools")
+install.packages("showtext")
+install.packages("scales")
+install.packages("grid")
+install.packages("lubridate")
+install.packages("activity")
+install.packages("stringr")
+install.packages("egg")
+install.packages("magick")
+install.packages("webshot")
+install.packages("gghalves")
+install.packages("tidyr")
+install.packages("ggforce")
+install.packages("zoo")
+install.packages("markdown")
+install.packages("kableExtra")
+
+# For Analysis
+install.packages("dplR")
+install.packages("spectr")
+install.packages("lomb")
+
+
+
